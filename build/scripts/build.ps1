9d734c05ee9d3210fde00f2d4c0a7164ae23f0bb
Nils Steenbock <nils.steenbock@gmail.com>
2020-01-08 21:44:01 +0100

    Reverting to use plain MsBuild call.

--- build/scripts/build.ps1
+++ build/scripts/build.ps1
@@ -1,7 +1,5 @@
 . $PSScriptRoot\config.ps1
 
-Write-Host "$msbuildexe"
-
 Get-ChildItem $PsScriptRoot\..\..\src -Recurse -Filter *.sln | foreach {
-    & $msbuildexe $_.FullName
+    MsBuild $_.FullName
 }
