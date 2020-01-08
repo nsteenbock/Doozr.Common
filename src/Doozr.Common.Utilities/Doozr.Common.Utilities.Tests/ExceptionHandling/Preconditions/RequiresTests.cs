d87d9729505ca96f5c8cde2f6371517cfe2d8f12
Nils Steenbock <nils.steenbock@gmail.com>
2020-01-08 22:19:09 +0100

    Adding first functionality with test.

---
+++ src/Doozr.Common.Utilities/Doozr.Common.Utilities.Tests/ExceptionHandling/Preconditions/RequiresTests.cs
@@ -0,0 +1,15 @@
+﻿using Doozr.Common.Utilities.ExceptionHandling.Preconditions;
+using System;
+using Xunit;
+
+namespace Doozr.Common.Utilities.Tests.ExceptionHandling.Preconditions
+{
+	public class RequiresTests
+	{
+		[Fact]
+		public void NotNull_ArgumentIsNullAndArgumentNameValid()
+		{
+			Assert.ThrowsAny<ArgumentNullException>(() => Requires.NotNull(null, "someArgumentName"));
+		}
+	}
+}
---
+++ src/Doozr.Common.Utilities/Doozr.Common.Utilities/ExceptionHandling/Preconditions/Requires.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Doozr.Common.Utilities.ExceptionHandling.Preconditions
+{
+	public static class Requires
+	{
+		public static void NotNull(object argument, string argumentName)
+		{
+			if (argument == null)
+			{
+				throw new ArgumentNullException(argumentName);
+			}
+		}
+	}
+}
