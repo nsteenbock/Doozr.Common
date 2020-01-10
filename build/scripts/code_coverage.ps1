c4952abde888f9efdc962403c1b91f8e3e83d188
nsteenbock <nils.steenbock@gmail.com>
2020-01-10 16:41:12 +0100

    Debug

--- build/scripts/code_coverage.ps1
+++ build/scripts/code_coverage.ps1
@@ -9,8 +9,10 @@ Get-ChildItem $PsScriptRoot\..\..\src -Recurse -Filter *.Tests.dll |
     }
 
 New-Item -ItemType Directory -Force -Path "$codeCoverageOutputDir"
-Write-Host $opencoverconsoleexe -target:$PsScriptRoot\run_tests.cmd  -filter:"+[$codeCoverageNamespacePrefix.*]* -[*.Tests]*" -register:user -searchdirs:"$searchDirs" -output:"$codeCoverageOutputDir\\results.xml"
-& $opencoverconsoleexe -target:$PsScriptRoot\run_tests.cmd  -filter:"+[$codeCoverageNamespacePrefix.*]* -[*.Tests]*" -register:user -searchdirs:"$searchDirs" -output:"$codeCoverageOutputDir\\results.xml"
+#Write-Host $opencoverconsoleexe -target:$PsScriptRoot\run_tests.cmd  -filter:"+[$codeCoverageNamespacePrefix.*]* -[*.Tests]*" -register:user -searchdirs:"$searchDirs" -output:"$codeCoverageOutputDir\\results.xml"
+#& $opencoverconsoleexe -target:$PsScriptRoot\run_tests.cmd  -filter:"+[$codeCoverageNamespacePrefix.*]* -[*.Tests]*" -register:user -searchdirs:"$searchDirs" -output:"$codeCoverageOutputDir\\results.xml"
+
+C:\projects\doozr-common\build\scripts\..\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:C:\projects\doozr-common\build\scripts\run_tests.cmd -filter:"+[Doozr.*]* -[*.Tests]*" -register:user -searchdirs:C:\projects\doozr-common\src\Doozr.Common.Utilities\Doozr.Common.Utilities.Tests\bin\Debug -output:C:\projects\doozr-common\build\scripts\..\coverage\results.xml
 
 if (Test-Path 'env:APPVEYOR_BUILD_NUMBER') {
     & $coverallsconsoleexe  --opencover -i "$codeCoverageOutputDir\\results.xml" --serviceNumber $env:APPVEYOR_BUILD_NUMBER
