66fa0469402639185f30f3ce03705b4d4fa9d6d2
Nils Steenbock <nils.steenbock@gmail.com>
2020-01-10 20:47:08 +0100

    Fixing build scripts.

--- build/scripts/code_coverage.ps1
+++ build/scripts/code_coverage.ps1
@@ -2,18 +2,19 @@
 
 $searchDirs="";
 Get-ChildItem $PsScriptRoot\..\..\src -Recurse -Filter *.Tests.dll |
-    ? { $_.FullName -notmatch "obj\\" } |
-    % {
-        Get-ChildItem $_.Directory
+    Where-Object { $_.FullName -notmatch "obj\\" } |
+    ForEach-Object {
         $searchDirs = $searchDirs + $_.Directory + ";"
     }
 
 New-Item -ItemType Directory -Force -Path "$codeCoverageOutputDir"
-#Write-Host $opencoverconsoleexe -target:$PsScriptRoot\run_tests.cmd  -filter:"+[$codeCoverageNamespacePrefix.*]* -[*.Tests]*" -register:user -searchdirs:"$searchDirs" -output:"$codeCoverageOutputDir\\results.xml"
-#& $opencoverconsoleexe -target:$PsScriptRoot\run_tests.cmd  -filter:"+[$codeCoverageNamespacePrefix.*]* -[*.Tests]*" -register:user -searchdirs:"$searchDirs" -output:"$codeCoverageOutputDir\\results.xml"
 
-C:\projects\doozr-common\build\scripts\..\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:run_tests.cmd -filter:"+[Doozr.*]* -[*.Tests]*" -register:appveyor -searchdirs:..\..\src\Doozr.Common.Utilities\Doozr.Common.Utilities.Tests\bin\Debug -output:..\coverage\results.xml
+& $opencoverconsoleexe -target:$PsScriptRoot\run_tests.cmd `
+    -filter:"+[$codeCoverageNamespacePrefix.*]* -[*.Tests]*" `
+    -register:$codeCoverageProfilerUser `
+    -searchdirs:"$searchDirs" `
+    -output:"$codeCoverageOutputDir\\results.xml"
 
-if (Test-Path 'env:APPVEYOR_BUILD_NUMBER') {
-    & $coverallsconsoleexe  --opencover -i "$codeCoverageOutputDir\\results.xml" --serviceNumber $env:APPVEYOR_BUILD_NUMBER
+if ($publishCodeCoverage) {
+    & $coverallsconsoleexe  --opencover -i "$codeCoverageOutputDir\\results.xml" --serviceNumber $buildNumber
 }
