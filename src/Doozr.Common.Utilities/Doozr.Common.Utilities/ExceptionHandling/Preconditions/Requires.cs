14301226000fc5b463ea55cb210e41713f13c2d3
Nils Steenbock <nils.steenbock@gmail.com>
2020-01-10 21:23:04 +0100

    Fixing build bug.

--- src/Doozr.Common.Utilities/Doozr.Common.Utilities/ExceptionHandling/Preconditions/Requires.cs
+++ src/Doozr.Common.Utilities/Doozr.Common.Utilities/ExceptionHandling/Preconditions/Requires.cs
@@ -5,7 +5,7 @@ namespace Doozr.Common.Utilities.ExceptionHandling.Preconditions
 	public static class Requires
 	{
 		public static void NotNull(object argument, string argumentName)
-		{Oops
+		{
 			if (argument == null)
 			{
 				throw new ArgumentNullException(argumentName);
