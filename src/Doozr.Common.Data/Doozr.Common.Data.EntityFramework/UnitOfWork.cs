b11935bad829ebdb9dd94dd92d0693ce9128fa44
Nils Steenbock <nils.steenbock@gmail.com>
2020-04-25 21:01:07 +0200

    Adding repository dictionary to UnitOfWork.

--- src/Doozr.Common.Data/Doozr.Common.Data.EntityFramework/UnitOfWork.cs
+++ src/Doozr.Common.Data/Doozr.Common.Data.EntityFramework/UnitOfWork.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 
 namespace Doozr.Common.Data.EntityFramework
 {
@@ -6,9 +8,37 @@ namespace Doozr.Common.Data.EntityFramework
 	{
 		protected readonly DbContext Context;
 
+		private readonly object lockObject = new object();
+
+		private Dictionary<Type, object> repositories = new Dictionary<Type, object>();
+
+		private readonly Type[] repositoryConstructorArgTypes = new Type[] { typeof(DbContext) };
+
+		private readonly object[] repositoryConstructorArgs;
+
+		protected T GetRepository<T>() where T: class
+		{
+			var repositoryType = typeof(T);
+
+			if (!repositories.ContainsKey(repositoryType))
+			{
+				lock(lockObject)
+				{
+					if (!repositories.ContainsKey(repositoryType))
+					{
+						var constructor = repositoryType.GetConstructor(repositoryConstructorArgTypes);
+						repositories.Add(repositoryType, constructor.Invoke(repositoryConstructorArgs));
+					}
+				}
+			}
+
+			return (T)repositories[repositoryType];
+		}
+
 		public UnitOfWork(DbContext context)
 		{
 			this.Context = context;
+			repositoryConstructorArgs = new object[] { context };
 		}
 
 		public int Complete()
