bafc64ae2ab69d0095be7240acf2e30c7018fd60
Nils Steenbock <nils.steenbock@gmail.com>
2020-09-02 21:55:25 +0200

    Added Doozr.Common.Desktop.Wpf library.

---
+++ src/Doozr.Common.Application.Desktop.Wpf/Doozr.Common.Application.Desktop.Wpf.Contracts/IWindowPlacementManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Doozr.Common.Application.Desktop.Wpf
+{
+	public interface IWindowPlacementManager
+	{
+		string GetWindowPlacement(Window window);
+
+		void SetWindowPlacement(Window window, string placement);
+	}
+}
---
+++ src/Doozr.Common.Application.Desktop.Wpf/Doozr.Common.Application.Desktop.Wpf/WindowPlacementManager.cs
@@ -0,0 +1,21 @@
+﻿using Doozr.Common.Application.Desktop.Window.Interop;
+using System;
+using System.Windows.Interop;
+
+namespace Doozr.Common.Application.Desktop.Wpf
+{
+	public class WindowPlacementManager: IWindowPlacementManager
+	{
+		public string GetWindowPlacement(System.Windows.Window window)
+		{
+			IntPtr handle = new WindowInteropHelper(window).Handle;
+			return WindowPlacementFunctions.GetPlacement(handle);
+		}
+
+		public void SetWindowPlacement(System.Windows.Window window, string placement)
+		{
+			IntPtr handle = new WindowInteropHelper(window).Handle;
+			WindowPlacementFunctions.SetPlacement(handle, placement);
+		}
+	}
+}
