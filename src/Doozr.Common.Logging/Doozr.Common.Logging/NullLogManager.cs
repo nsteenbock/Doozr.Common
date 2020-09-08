41a8d57741aba68c1f2cad674016e810270c529c
Nils Steenbock <nils.steenbock@gmail.com>
2020-09-08 22:04:45 +0200

    Adding NullLogManager.

---
+++ src/Doozr.Common.Logging/Doozr.Common.Logging/NullLogManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Doozr.Common.Logging
+{
+	public class NullLogManager : LogManager
+	{
+		static ILogger loggerInstance = new NullLogger();
+
+		public override ILogger GetLogger<T>()
+		{
+			return loggerInstance;
+		}
+
+		class NullLogger : Logger
+		{
+			public override void EnterMethod(string methodName) { }
+
+			public override void EnterMethod(LogLevel logLevel, string methodName) { }
+
+
+			public override void EnterMethod(object instance, string methodName) { }
+
+			public override void EnterMethod(LogLevel logLevel, object instance, string methodName) { }
+
+			public override void LeaveMethod(LogLevel logLevel, string methodName) { }
+
+			public override void LeaveMethod(string methodName) { }
+
+			public override void LeaveMethod(object instance, string methodName) { }
+
+			public override void LeaveMethod(LogLevel logLevel, object instance, string methodName) { }
+
+			public override void Log(string message) { }
+
+			public override void Log(LogLevel logLevel, string message) { }
+
+			public override void LogException(Exception exception) { }
+
+			public override void LogException(string message, Exception exception) { }
+
+			public override void LogWarning(string message) { }
+		}
+	}
+}
