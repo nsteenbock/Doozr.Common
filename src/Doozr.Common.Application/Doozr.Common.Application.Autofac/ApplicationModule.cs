c92fb2f10046c740cde1214ba4977577673e2566
Nils Steenbock <nils.steenbock@gmail.com>
2020-09-20 21:45:47 +0200

    Added autofac registration for application library.

---
+++ src/Doozr.Common.Application/Doozr.Common.Application.Autofac/ApplicationModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+
+namespace Doozr.Common.Application
+{
+	public class ApplicationModule: Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterType<ApplicationProperties>().As<IApplicationProperties>().SingleInstance();
+			builder.RegisterType<ApplicationDataStore>().As<IApplicationDataStore>().SingleInstance();
+			builder.RegisterType<ProcessNamePortableApplicationDetector>().As<IPortableApplicationDetector>().SingleInstance();
+		}
+	}
+}
