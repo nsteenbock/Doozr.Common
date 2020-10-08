41d2f495761c53627174ffdb4ebe19e4b50f209f
Nils Steenbock <nils.steenbock@gmail.com>
2020-10-08 21:21:01 +0200

    Fixed infinite recursion.

--- src/Doozr.Common.Logging/Doozr.Common.Logging.Aspect/LoggingAttribute.cs
+++ src/Doozr.Common.Logging/Doozr.Common.Logging.Aspect/LoggingAttribute.cs
@@ -4,23 +4,31 @@ namespace Doozr.Common.Logging.Aspect
 {
 	public class LoggingAttribute: OnMethodBoundaryAspect
 	{
+		public static ILogManager LogManager{ get; set; }
+
 		public override void OnEntry(MethodExecutionArgs arg)
 		{
+			if (arg.Method.Name == "set_Logger" || arg.Method.Name == "get_Logger") return;
 			var loggingInstance = (arg.Instance as ILoggingObject);
-			loggingInstance?.Logger.EnterMethod(arg.Method.Name);
+			var logger = loggingInstance?.Logger ?? LogManager?.GetLogger(arg.Instance.GetType());
+			logger?.EnterMethod(arg.Method.Name);
 		}
 
 		public override void OnExit(MethodExecutionArgs arg)
 		{
+			if (arg.Method.Name == "set_Logger" || arg.Method.Name == "get_Logger") return;
 			var loggingInstance = (arg.Instance as ILoggingObject);
-			loggingInstance?.Logger.LeaveMethod(arg.Method.Name);
+			var logger = loggingInstance?.Logger ?? LogManager?.GetLogger(arg.Instance.GetType());
+			logger?.LeaveMethod(arg.Method.Name);
 		}
 
 		public override void OnException(MethodExecutionArgs arg)
 		{
+			if (arg.Method.Name == "set_Logger" || arg.Method.Name == "get_Logger") return;
 			var loggingInstance = (arg.Instance as ILoggingObject);
-			loggingInstance?.Logger.LogException(arg.Exception);
-			loggingInstance?.Logger.LeaveMethod(arg.Method.Name);
+			var logger = loggingInstance?.Logger ?? LogManager?.GetLogger(arg.Instance.GetType());
+			logger?.LogException(arg.Exception);
+			logger?.LeaveMethod(arg.Method.Name);
 		}
 	}
 }
