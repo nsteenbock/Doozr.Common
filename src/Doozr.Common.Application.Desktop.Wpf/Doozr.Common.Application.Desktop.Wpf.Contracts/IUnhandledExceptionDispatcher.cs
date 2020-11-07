a94fc9a335f8a5fc49f21ca249a88a7605e3a25b
Nils Steenbock <nils.steenbock@gmail.com>
2020-11-07 21:13:14 +0100

    Added UnhandledExceptionDispatcher.

---
+++ src/Doozr.Common.Application.Desktop.Wpf/Doozr.Common.Application.Desktop.Wpf.Contracts/IUnhandledExceptionDispatcher.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Doozr.Common.Application.Desktop.Wpf
+{
+	public interface IUnhandledExceptionDispatcher
+	{
+		event Action<Exception> Handler;
+	}
+}
---
+++ src/Doozr.Common.Application.Desktop.Wpf/Doozr.Common.Application.Desktop.Wpf/UnhandledExceptionDispatcher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Doozr.Common.Application.Desktop.Wpf
+{
+	public class UnhandledExceptionDispatcher : IUnhandledExceptionDispatcher
+	{
+		public event Action<Exception> Handler;
+
+		public UnhandledExceptionDispatcher()
+		{
+			System.Windows.Application.Current.DispatcherUnhandledException += ApplicationDispatcherUnhandledException;
+			AppDomain.CurrentDomain.UnhandledException += AppDomainUnhandledException;
+			TaskScheduler.UnobservedTaskException += TaskSchedulerUnobservedTaskException;
+		}
+
+		private void TaskSchedulerUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+		{
+			Handler?.Invoke(e.Exception);
+			Environment.Exit(1);
+		}
+
+		private void AppDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
+		{
+			Handler?.Invoke(e.ExceptionObject as Exception);
+			Environment.Exit(1);
+		}
+
+		private void ApplicationDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+		{
+			Handler?.Invoke(e.Exception);
+			e.Handled = true;
+			System.Windows.Application.Current.Shutdown(1);
+		}
+	}
+}
