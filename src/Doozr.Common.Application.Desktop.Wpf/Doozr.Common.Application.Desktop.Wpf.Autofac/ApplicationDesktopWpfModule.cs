5cac73fc61aee18bd7a3f2011b07d11b4bf796db
Nils Steenbock <nils.steenbock@gmail.com>
2020-11-17 10:29:59 +0100

    Updating nuget packages and adding support for autofac to desktop wpf.

---
+++ src/Doozr.Common.Application.Desktop.Wpf/Doozr.Common.Application.Desktop.Wpf.Autofac/ApplicationDesktopWpfModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+
+namespace Doozr.Common.Application.Desktop.Wpf.Autofac
+{
+	public class ApplicationDesktopWpfModule: Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterType<WindowPlacementManager>().As<IWindowPlacementManager>().SingleInstance();
+			builder.RegisterType<WindowPlacementPersistor>().As<IWindowPlacementPersistor>().SingleInstance();
+		}
+	}
+}
