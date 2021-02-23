7ce1a4142da3585fe67421f456876b61ee20555d
Nils Steenbock <nils.steenbock@gmail.com>
2021-02-23 21:24:11 +0100

    Added missing test classes.

---
+++ src/Doozr.Common.Application/Doozr.Common.Application.Tests/TestData/SettingWithNotifyPropertyChanged.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace Doozr.Common.Application.Tests.TestData
+{
+	public class SettingWithNotifyPropertyChanged : INotifyPropertyChanged
+	{
+		private int value;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public int Value
+		{
+			get => value;
+			set
+			{
+				this.value = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+			}
+		}
+	}
+}
---
+++ src/Doozr.Common.Application/Doozr.Common.Application.Tests/TestData/SettingWithoutNotifyPropertyChanged.cs
@@ -0,0 +1,7 @@
+﻿namespace Doozr.Common.Application.Tests.TestData
+{
+	public class SettingWithoutNotifyPropertyChanged
+	{
+		public int Value{ get; set; }
+	}
+}
